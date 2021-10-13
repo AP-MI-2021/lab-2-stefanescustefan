79cbf2891f41d64df12118760eed7c61c1287309
stefan19 <stefan.stefanescu@stud.ubbcluj.ro>
2021-10-13 11:51:56 +0300

    Lab 2 - problemele 6, 8, 9

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def is_prime(n):
     """
     Determina daca un numar este prim
+
     :param n: un numar natural
     :return: True daca n este prim, altfel False
     """
@@ -58,13 +59,33 @@ def test_get_base_2():
     assert get_base_2("287") == "100011111"
 
 
+def get_base_16_from_2(n):
+    """
+    Transforma un numar dat din baza 2 in baza 16
+
+    :param n: Un numar scris in baza 2
+    :return: Reprezentarea in baza 16 a numarului dat
+    """
+    x = int(n, 2)
+    return str(hex(x)[2:])
+
+
+def test_get_base_16_from_2():
+    assert get_base_16_from_2("10") == "2"
+    assert get_base_16_from_2("101") == "5"
+    assert get_base_16_from_2("1111") == "f"
+    assert get_base_16_from_2("11101") == "1d"
+
+
 def main():
     test_is_superprime()
     test_get_base_2()
+    test_get_base_16_from_2()
     end = False
     while not end:
         print("1. Verificati daca un numar este superprim")
         print("2. Convertiti un numar din baza 10 in baza 2")
+        print("3. Convertiti un numar din baza 2 in baza 16")
         print("x. Iesire")
         optiune = input()
         if optiune == '1':
@@ -76,6 +97,9 @@ def main():
         elif optiune == '2':
             n = input("Introduceti un numar natural: ")
             print(f"{n} in baza 2: {get_base_2(n)}")
+        elif optiune == '3':
+            n = input("Introduceti un numar in baza 2: ")
+            print(f"{n} in baza 16: {get_base_16_from_2(n)}")
         elif optiune == 'x':
             end = True
         else:
